25aa178234bb5d75d17df43b6eba1e24f7b24675
BillyNoble2 <103219865+BillyNoble2@users.noreply.github.com>
2023-03-10 17:12:45 +0000

    Updated start game function.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
-console.log(cards)
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
